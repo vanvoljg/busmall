a67d8fe68594b9397d0d604fb61798b0658b5731
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-02-19 10:03:05 -0800

    styling and a couple global js vars

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@
 
 /*
 
-
+need globals:
+click_limit
+image array
+state array/object
+event on image section
 
 */
+
+// Global variables
+
+var click_limit = 25;
+var image_list = [];
+var displayed_images = [];
+ 
 
 var init = function() {
   // structure the init function
